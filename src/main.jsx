0e650fa7fbfabb47abffd8f831f20b87ea36d7b2
CharanSala <salacharan81@gmail.com>
2026-01-19 17:24:12 +0530

    Fix Router inside Router bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+const basename = import.meta.env.PROD ? "/Portifolio" : "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Portifolio">
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>,
 );
