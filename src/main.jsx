eb53bf8f805c2099c477545e1db96f9500e4afa2
CharanSala <salacharan81@gmail.com>
2026-01-19 17:26:53 +0530

    Fix GitHub Pages base and router basename

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const basename = import.meta.env.PROD ? "/Portifolio" : "/";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter basename="/Portifolio">
     <App />
   </BrowserRouter>,
 );
